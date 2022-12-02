edf9512d34a1fa79eb7ee745a7a1f1f94b21c0ff
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-12-02 15:59:31 +0600

    configure websockets

--- backend/app/Models/Admin/Post.php
+++ backend/app/Models/Admin/Post.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Admin;
 
+use App\Events\PostCreated;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'created' => PostCreated::class,
+    ];
 }
